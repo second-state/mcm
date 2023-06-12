6371ab7b3029425d75a853a0385452f8ee9e0245
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-06-12 13:06:47 +0800

    init
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «mcm» {
+  -- add package configuration options here
+}
+
+lean_lib «Mcm» {
+  -- add library configuration options here
+}
+
+@[default_target]
+lean_exe «mcm» {
+}
